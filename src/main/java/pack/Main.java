be9924791494bef4156224368e4ed2edd6291ce2
DESKTOP-2ACJTHO\Fresher <magicghost.vu@gmail.com>
2017-06-01 17:40:45 +0700

    #add api convert msg and baseCmd

--- src/main/java/pack/Main.java
+++ src/main/java/pack/Main.java
@@ -6,6 +6,7 @@ import sendToServer.cmd.BaseCmd;
 import sendToServer.cmd.HandShakeCmd;
 import sendToServer.msg.Message;
 import utils.ByteArray;
+import utils.MessageUtils;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -20,7 +21,7 @@ import java.util.Iterator;
  */
 public class Main {
 
-    public static void sendCmdToServer(BaseCmd cmd){
+    public static void sendCmdToServer(BaseCmd cmd) {
 
     }
 
@@ -29,54 +30,21 @@ public class Main {
         InetSocketAddress socketAddress = new InetSocketAddress("49.213.81.43", 10002);
         SocketChannel socketChannel = SocketChannel.open(socketAddress);
         socketChannel.configureBlocking(false);
-
-
         HandShakeCmd handShakeCmd = new HandShakeCmd("");
-
-        Message message = new Message();
-        message.setTargetController(handShakeCmd.getControllerId());
-        message.setId(handShakeCmd.getTypeId());
-
-        if (handShakeCmd.createBody()) {
-            message.setContent(handShakeCmd.getBody());
-        }
-
-        ByteArray recreateByteArray = new ByteArray();
-        recreateByteArray.writeByte(message.getTargetController());
-        recreateByteArray.writeShort((short) message.getId());
-        recreateByteArray.writeBytes(message.getContent().getBytes());
-
-        message.setContent(recreateByteArray);
-
-
-        //send to server via bytes
-        int packageHeaderEncoded = -128;
-        ByteArray binData = message.getContent();
-
-        ByteArray array = new ByteArray();
-        array.writeByte(packageHeaderEncoded);
-
-        array.writeShort((short) binData.getLength());
-        array.writeBytes(binData.getBytes());
-
-        ByteBuffer targetBuffer = ByteBuffer.wrap(array.getBytes());
-
+        Message message= MessageUtils.convertBaseCmdToMessage(handShakeCmd);
+        ByteBuffer targetBuffer= MessageUtils.convertMessageToBuffer(message);
         socketChannel.write(targetBuffer);
         targetBuffer.clear();
-
-
         Selector selector = Selector.open();
         int operation = SelectionKey.OP_CONNECT | SelectionKey.OP_READ | SelectionKey.OP_WRITE;
-
         socketChannel.register(selector, operation);
-
         //selector.
 
         while (true) {
             int numberKey = selector.select();
             //System.out.println("");
             Iterator<SelectionKey> allKeySelected = selector.selectedKeys().iterator();
-            allKeySelected.forEachRemaining( keySelected -> {
+            allKeySelected.forEachRemaining(keySelected -> {
                 try {
                     allKeySelected.remove();
                     SocketChannel currentChannel = (SocketChannel) keySelected.channel();
@@ -91,6 +59,8 @@ public class Main {
                         ByteBuffer target = ByteBuffer.wrap(tmpByteBuffer.array());
                         OriginMessage oMessage = new OriginMessage(target);
                         System.out.println("bin data length :" + oMessage.getBinDataLength());
+
+                        //line 96 need to be refactor, logic fail here
                         HandshakeMsg handshakeMsg = new HandshakeMsg(oMessage.getData());
                         System.out.println(handshakeMsg.getSessionToken());
                     }
--- src/main/java/utils/MessageUtils.java
+++ src/main/java/utils/MessageUtils.java
@@ -11,36 +11,40 @@ import java.nio.ByteBuffer;
 public class MessageUtils {
 
 
-    static int packageHeaderEncoded= -128;
+    static int packageHeaderEncoded = -128;
 
-    public static ByteBuffer convertMessageToBuffer(Message message)  {
+    public static ByteBuffer convertMessageToBuffer(Message message) {
         try {
-            ByteArray tmpByteArray= new ByteArray();
+            ByteArray tmpByteArray = new ByteArray();
             tmpByteArray.writeByte(message.getTargetController());
-            tmpByteArray.writeShort((short)message.getId());
+            tmpByteArray.writeShort((short) message.getId());
             tmpByteArray.writeBytes(message.getContent().getBytes());
             //message.setContent(tmpByteArray);
-
-            ByteArray targetByteArr= new ByteArray();
+            ByteArray targetByteArr = new ByteArray();
             targetByteArr.writeByte(packageHeaderEncoded);
             targetByteArr.writeShort((short) tmpByteArray.getLength());
             targetByteArr.writeBytes(tmpByteArray.getBytes());
-
-
-
-
-        }catch (Exception e){
-
+            ByteBuffer result = ByteBuffer.wrap(targetByteArr.getBytes());
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-
-
-
-
-        return null;
 
     }
 
-    public static Message convertBaseCmdToMessage(BaseCmd cmd){
-        return null;
+    public static Message convertBaseCmdToMessage(BaseCmd cmd) {
+
+        Message result= new Message();
+        result.setTargetController(cmd.getControllerId());
+        result.setId(cmd.getTypeId());
+        if(cmd.createBody()){
+            result.setContent(cmd.getBody());
+            return result;
+        }else{
+            throw new IllegalArgumentException("can not create body"+ cmd);
+            //return null;
+        }
+
     }
 }
